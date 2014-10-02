06ec5dc95d567905e27c67523efad5cc52558990
Joshua Granick <jgranick@users.noreply.github.com>
2014-10-02 11:37:33 -0700

    Add USE_SWF_EXPORTER_SERVICE

--- build.bat
+++ build.bat
@@ -1,13 +1,23 @@
+@echo off
+SetLocal EnableDelayedExpansion
 @echo on
 
 cd project
+
+@echo off
+if "%1" == "clean" (
+	@echo on
+	haxelib run hxcpp Build.xml clean -Dwindows -DHXCPP_M64
+)
+@echo on
+
 haxelib run hxcpp Build.xml -Dwindows -DHXCPP_M64
 
 @echo off
 if %errorlevel% neq 0 (
 	cd ..
 	exit /b %errorlevel%
-} else ()
+)
 @echo on
 
 cd ..
