a089724e1b98f5f10a346810dec5af2246a63c3e
Joshua Granick <jgranick@users.noreply.github.com>
2015-01-15 01:17:10 -0800

    Remove OpenFLPlugin folder before copying, Flash CC is not detecting changes otherwise

--- build.sh
+++ build.sh
@@ -12,4 +12,5 @@ haxelib run hxcpp Build.xml -Dmac -DHXCPP_M64
 cd ..
 mkdir -p ~/Library/Application\ Support/Adobe/CEP/extensions/
 mv OpenFLPlugin/plugin/lib/mac/OpenFLPlugin.fcm.plugin/Contents/MacOS/OpenFLPlugin.dylib OpenFLPlugin/plugin/lib/mac/OpenFLPlugin.fcm.plugin/Contents/MacOS/OpenFLPlugin
+rm -rf ~/Library/Application\ Support/Adobe/CEP/extensions/OpenFLPlugin
 cp -rf OpenFLPlugin ~/Library/Application\ Support/Adobe/CEP/extensions/OpenFLPlugin
