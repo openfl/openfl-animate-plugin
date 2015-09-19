1c716efa1d60eac554e634f842e482a4c53d3b89
Joshua Granick <jgranick@users.noreply.github.com>
2015-09-18 18:40:00 -0700

    Fix Mac install script

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ haxelib run hxcpp Build.xml -Dmac -DHXCPP_M64
 
 cd ..
 mkdir -p ~/Library/Application\ Support/Adobe/CEP/extensions/
-mv OpenFLPlugin/plugin/lib/mac/OpenFLPlugin.fcm.plugin/Contents/MacOS/OpenFLPlugin.dylib OpenFLPlugin/plugin/lib/mac/OpenFLPlugin.fcm.plugin/Contents/MacOS/OpenFLPlugin
+mv OpenFLPlugin/plugin/lib/mac/OpenFLPlugin.fcm.plugin/Contents/MacOS/OpenFLPlugin OpenFLPlugin/plugin/lib/mac/OpenFLPlugin.fcm.plugin/Contents/MacOS/OpenFLPlugin
 rm -rf ~/Library/Application\ Support/Adobe/CEP/extensions/OpenFLPlugin
 cp -rf OpenFLPlugin ~/Library/Application\ Support/Adobe/CEP/extensions/OpenFLPlugin
