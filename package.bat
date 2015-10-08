0d786d18ee1ae2d05a26ce63099250d5c4dc4644
Joshua Granick <jgranick@users.noreply.github.com>
2015-10-08 15:10:23 -0700

    Use updated package script

--- package.bat
+++ package.bat
@@ -2,14 +2,6 @@
 SetLocal EnableDelayedExpansion
 @echo on
 
-if not exist ".staged-extension" mkdir .staged-extension
-if exist ".staged-extension\OpenFLPlugin" rmdir /s /q .staged-extension\OpenFLPlugin
+if exist "OpenFLPlugin.zxp" del /f OpenFLPlugin.zxp
 
-xcopy OpenFLPlugin .staged-extension\OpenFLPlugin /s /i /y
-
-copy /y icon.png .staged-extension\icon.png
-copy /y org.openfl.OpenFLPlugin.mxi .staged-extension\org.openfl.OpenFLPlugin.mxi
-
-if exist "org.openfl.OpenFLPlugin.zxp" del /f org.openfl.OpenFLPlugin.zxp
-
-ZXPSignCmd -sign .staged-extension org.openfl.OpenFLPlugin.zxp certificate.p12 "%1" -tsa https://timestamp.geotrust.com/tsa
+ZXPSignCmd -sign OpenFLPlugin OpenFLPlugin.zxp certificate.p12 "%1" -tsa https://timestamp.geotrust.com/tsa
