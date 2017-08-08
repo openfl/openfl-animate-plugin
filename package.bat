17bcaa4ac9e9446c41f4499b951646d87b57f582
Joshua Granick <jgranick@users.noreply.github.com>
2017-08-07 18:27:16 -0700

    Update packaging script

--- package.bat
+++ package.bat
@@ -4,4 +4,4 @@ SetLocal EnableDelayedExpansion
 
 if exist "OpenFLPlugin.zxp" del /f OpenFLPlugin.zxp
 
-ZXPSignCmd -sign OpenFLPlugin OpenFLPlugin.zxp certificate.p12 "%1" -tsa https://timestamp.geotrust.com/tsa
+ZXPSignCmd -sign OpenFLPlugin OpenFLPlugin.zxp certificate.p12 "%1" -tsa http://timestamp.globalsign.com/scripts/timstamp.dll
